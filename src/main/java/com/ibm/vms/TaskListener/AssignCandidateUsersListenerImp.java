a6358631beac11ddb81ec0647fb0924c09a6ccbd
microservicesengineer <microengineer@126.com>
2019-07-15 06:58:32 +0800

    change the class
    change the class

--- src/main/java/com/ibm/vms/TaskListener/AssignCandidateUsersListenerImp.java
+++ src/main/java/com/ibm/vms/TaskListener/AssignCandidateUsersListenerImp.java
@@ -1,17 +1,19 @@
 package com.ibm.vms.TaskListener;
 
+import java.io.Serializable;
 import java.util.Arrays;
 
 import org.activiti.engine.delegate.DelegateExecution;
 import org.activiti.engine.delegate.DelegateTask;
 import org.activiti.engine.delegate.ExecutionListener;
 import org.activiti.engine.delegate.Expression;
+import org.activiti.engine.delegate.JavaDelegate;
 import org.activiti.engine.delegate.TaskListener;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
-public class AssignCandidateUsersListenerImp implements TaskListener, ExecutionListener {
+public class AssignCandidateUsersListenerImp implements ExecutionListener {
 
 	/**
 	 * 
@@ -19,11 +21,11 @@ public class AssignCandidateUsersListenerImp implements TaskListener, ExecutionL
 	private static final long serialVersionUID = 1L;
 
 	private Expression category;
-	
-	private Expression group;
+
+	private Expression type;
 
 	public enum PROCESS_CATEGORY {
-		
+
 		LEAVE("LEAVE");
 
 		private String name;
@@ -36,22 +38,48 @@ public class AssignCandidateUsersListenerImp implements TaskListener, ExecutionL
 			return name;
 		}
 	}
-	
-	@Override
-	public void notify(DelegateTask delegateTask) {
-		// TODO: fetch users from business DB
-		String processtype = category.getExpressionText().toString();
-		log.info("the candicate type is: {}", processtype);
-		
-		String processgroup = group.getExpressionText().toString();
-		log.info("the candicate group is: {}", processgroup);
-		
-		delegateTask.setVariable("pers", Arrays.asList("1", "2"));
-	}
+
+//	 @Override
+//	 public void notify(DelegateTask delegateTask) {
+//	log.info("the event in task listener type is: {}",  delegateTask.getEventName());
+//	 // TODO: fetch users from business DB
+//	 String processtype = category.getExpressionText().toString();
+//	 log.info("the candicate type is: {}", processtype);
+//	
+//	 String processgroup = type.getExpressionText().toString();
+//	 log.info("the candicate type is: {}", processgroup);
+//	
+//	// delegateTask.setVariable("pers", Arrays.asList("1", "2"));
+//	 }
+
+//	@Override
+//	public void execute(DelegateExecution execution) {
+//		log.info("the event type in execution is: {}",  execution.getEventName());
+//		
+//		
+//		String processtype = category.getExpressionText().toString();
+//		log.info("the candicate type is: {}", processtype);
+//
+//		String processgroup = type.getExpressionText().toString();
+//		log.info("the candicate type is: {}", processgroup);
+//
+//		execution.setVariable("pers", Arrays.asList("1", "2"));
+//		
+//	}
 
 	@Override
 	public void notify(DelegateExecution execution) {
-		log.info("the candicate group is: {}");
 		
+		log.info("the event type in execution is: {}",  execution.getEventName());
+		
+		
+		String processtype = category.getExpressionText().toString();
+		log.info("the candicate type is: {}", processtype);
+
+		String processgroup = type.getExpressionText().toString();
+		log.info("the candicate type is: {}", processgroup);
+
+		execution.setVariable("pers", Arrays.asList("1", "2"));
+
 	}
 }
